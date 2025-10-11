b3e75b837e7ad11149f222acacd192c51ece8024
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-10-11 23:59:29 +0000

    Deploying to main from @ embeddedt/crowdmark-tweaks@7837e901b1428cd7ed75ea9b188e9309f223bda5 🚀

--- crowdmark-tweaks.meta.js
+++ crowdmark-tweaks.meta.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name Crowdmark Tweaks
 // @description Useful tweaks for Crowdmark
-// @version 0.1.0-build.2025-10-11T02:40:37.063Z
+// @version 0.1.0-build.2025-10-11T23:59:27.766Z
 // @author embeddedt
 // @homepage https://github.com/embeddedt/crowdmark-tweaks
 // @supportURL https://github.com/embeddedt/crowdmark-tweaks/issues
 // @match https://app.crowdmark.com/*
 // @connect app.crowdmark.com
+// @connect localhost
 // @downloadURL https://github.com/embeddedt/crowdmark-tweaks/raw/refs/heads/main/crowdmark-tweaks.user.js
 // @grant GM_addStyle
 // @grant GM_addElement
+// @grant GM_xmlhttpRequest
 // @icon https://www.google.com/s2/favicons?sz=64&domain=crowdmark.com
 // @namespace https://github.com/embeddedt
 // @updateURL https://github.com/embeddedt/crowdmark-tweaks/raw/refs/heads/main/crowdmark-tweaks.meta.js
